0c403e12f934d49af94126b73dc78dcd0c9c3ef4
Rishabh Jain <88618109+Rishie123@users.noreply.github.com>
2024-09-05 21:08:31 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pandas as pd
 
 # Load the combined DataFrame
-df = pd.read_csv('modified_ate_900.csv')
+df = pd.read_csv('bad_dates_ate_240.csv')
 
 
 # Initialize the Dash app
